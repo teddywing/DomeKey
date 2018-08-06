8645fbf85382c8ff4fe3befd946d41a62f837184
Teddy Wing <git@teddywing.com>
2018-08-06 23:05:38 +0200

    New Xcode 9.2 command-line tool project

---
+++ DomeKey/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DomeKey
+//
+//  Created by tw on 8/6/18.
+//  Copyright © 2018 tw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

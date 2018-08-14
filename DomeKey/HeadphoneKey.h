dbab68634814ebdabcc5dadfbc0bc2b82e5e112f
Teddy Wing <git@teddywing.com>
2018-08-14 12:15:02 +0200

    Add `HeadphoneKey` class
    This will be our delegate for the DDHID key listener.

---
+++ DomeKey/HeadphoneKey.h
@@ -0,0 +1,13 @@
+//
+//  HeadphoneKey.h
+//  DomeKey
+//
+//  Created by tw on 8/14/18.
+//  Copyright © 2018 tw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HeadphoneKey : NSObject
+
+@end
---
+++ DomeKey/HeadphoneKey.m
@@ -0,0 +1,13 @@
+//
+//  HeadphoneKey.m
+//  DomeKey
+//
+//  Created by tw on 8/14/18.
+//  Copyright © 2018 tw. All rights reserved.
+//
+
+#import "HeadphoneKey.h"
+
+@implementation HeadphoneKey
+
+@end

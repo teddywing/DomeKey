f3a108e3c8795af7694d2dbd9341b1fabbff9dd1
Teddy Wing <git@teddywing.com>
2018-08-27 11:21:37 +0200

    Add `KeyboardSimulator`
    A new class that will know how to simulate key presses in order to
    handle macros.

---
+++ DomeKey/KeyboardSimulator.h
@@ -0,0 +1,13 @@
+//
+//  KeyboardSimulator.h
+//  DomeKey
+//
+//  Created by tw on 8/27/18.
+//  Copyright © 2018 tw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KeyboardSimulator : NSObject
+
+@end
---
+++ DomeKey/KeyboardSimulator.m
@@ -0,0 +1,13 @@
+//
+//  KeyboardSimulator.m
+//  DomeKey
+//
+//  Created by tw on 8/27/18.
+//  Copyright © 2018 tw. All rights reserved.
+//
+
+#import "KeyboardSimulator.h"
+
+@implementation KeyboardSimulator
+
+@end

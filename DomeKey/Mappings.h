fb46a8b01ccf243b0caa360e360c43f1fc2df7de
Teddy Wing <git@teddywing.com>
2018-10-04 14:13:44 +0200

    Add a new `Mappings` class
    We'll reload the mappings file here.

---
+++ DomeKey/Mappings.h
@@ -0,0 +1,13 @@
+//
+//  Mappings.h
+//  DomeKey
+//
+//  Created by tw on 10/4/18.
+//  Copyright © 2018 tw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Mappings : NSObject
+
+@end
---
+++ DomeKey/Mappings.m
@@ -0,0 +1,13 @@
+//
+//  Mappings.m
+//  DomeKey
+//
+//  Created by tw on 10/4/18.
+//  Copyright © 2018 tw. All rights reserved.
+//
+
+#import "Mappings.h"
+
+@implementation Mappings
+
+@end

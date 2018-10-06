55cde050a168a09c2691081da38064821d492f66
Teddy Wing <git@teddywing.com>
2018-10-06 18:46:58 +0200

    Add DKApplication
    Will be used to override the `sendEvent:` method to try to intercept
    media key events as described in
    https://weblog.rogueamoeba.com/2007/09/29/.
    
    Trying to find out if intercepting these events will enable the program
    to work with Bluetooth headphones.

---
+++ DomeKey/DKApplication.h
@@ -0,0 +1,13 @@
+//
+//  DKApplication.h
+//  DomeKey
+//
+//  Created by tw on 10/6/18.
+//  Copyright © 2018 tw. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DKApplication : NSApplication
+
+@end
---
+++ DomeKey/DKApplication.m
@@ -0,0 +1,13 @@
+//
+//  DKApplication.m
+//  DomeKey
+//
+//  Created by tw on 10/6/18.
+//  Copyright © 2018 tw. All rights reserved.
+//
+
+#import "DKApplication.h"
+
+@implementation DKApplication
+
+@end

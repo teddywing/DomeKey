03c06d9ab75ce794b30a75c7a6314ff8371f4b68
Teddy Wing <git@teddywing.com>
2018-10-24 03:26:27 +0200

    Add `LicenseHandler`
    This class will encapsulate all license and trial handling.

---
+++ DomeKey/LicenseHandler.h
@@ -0,0 +1,13 @@
+//
+//  LicenseHandler.h
+//  DomeKey
+//
+//  Created by tw on 10/24/18.
+//  Copyright © 2018 tw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LicenseHandler : NSObject
+
+@end
---
+++ DomeKey/LicenseHandler.m
@@ -0,0 +1,13 @@
+//
+//  LicenseHandler.m
+//  DomeKey
+//
+//  Created by tw on 10/24/18.
+//  Copyright © 2018 tw. All rights reserved.
+//
+
+#import "LicenseHandler.h"
+
+@implementation LicenseHandler
+
+@end

40425fa0d211e879f93a282472b5a90906d9668f
Teddy Wing <git@teddywing.com>
2018-10-24 07:34:54 +0200

    Add an `XDG` class
    For interacting with XDG directories.

---
+++ DomeKey/XDG.h
@@ -0,0 +1,13 @@
+//
+//  XDG.h
+//  DomeKey
+//
+//  Created by tw on 10/24/18.
+//  Copyright © 2018 tw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XDG : NSObject
+
+@end
---
+++ DomeKey/XDG.m
@@ -0,0 +1,13 @@
+//
+//  XDG.m
+//  DomeKey
+//
+//  Created by tw on 10/24/18.
+//  Copyright © 2018 tw. All rights reserved.
+//
+
+#import "XDG.h"
+
+@implementation XDG
+
+@end

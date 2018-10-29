399079234f7f029da9d4acad2bc38d85a4f28e1e
Teddy Wing <git@teddywing.com>
2018-10-29 18:45:02 +0100

    Add `Sounds` class
    This will be used to play the mode activated & deactivated sounds via
    `NSSound` (hopefully).

---
+++ DomeKey/Sounds.h
@@ -0,0 +1,13 @@
+//
+//  Sounds.h
+//  DomeKey
+//
+//  Created by tw on 10/29/18.
+//  Copyright © 2018 tw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Sounds : NSObject
+
+@end
---
+++ DomeKey/Sounds.m
@@ -0,0 +1,13 @@
+//
+//  Sounds.m
+//  DomeKey
+//
+//  Created by tw on 10/29/18.
+//  Copyright © 2018 tw. All rights reserved.
+//
+
+#import "Sounds.h"
+
+@implementation Sounds
+
+@end
